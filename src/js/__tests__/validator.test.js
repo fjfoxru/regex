5fafaecef56df0e1c2f2cf788f36cc45f4307f0c
fjfoxru <61179622+fjfoxru@users.noreply.github.com>
2021-01-18 09:46:02 +0300

    Update validator.test.js

--- src/js/__tests__/validator.test.js
+++ src/js/__tests__/validator.test.js
@@ -2,7 +2,7 @@ import Validator from '../validator';
 
 test('Логин только из букв и цифр', () => {
   const testValid = new Validator();
-  const testUser = testValid.validateUsername('user2020user')
+  const testUser = testValid.validateUsername('user2u2s4er')
 
   expect(testUser).toBe(true);
 });
@@ -24,6 +24,13 @@ test('Логин не валидный, если цифра в конце', () =
 test('Логин не валидный, если цифра первая', () => {
   const testValid = new Validator();
   const testUser = testValid.validateUsername('3user')
+
+  expect(testUser).toBe(false);
+});
+
+test('Нельзя поставить больше 3 цифр подряд', () => {
+  const testValid = new Validator();
+  const testUser = testValid.validateUsername('u445553ser')
 
   expect(testUser).toBe(false);
 });
