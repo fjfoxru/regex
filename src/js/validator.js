cc24223486605282cb385535ab5a58f469112618
fjfoxru <61179622+fjfoxru@users.noreply.github.com>
2021-01-18 09:45:20 +0300

    Update validator.js

--- src/js/validator.js
+++ src/js/validator.js
@@ -1,5 +1,7 @@
 export default class Validator {
     validateUsername(username) {
-        return /^[a-zA-Z][a-zA-Z0-9-_]*(?!\d{4,})[a-zA-Z]$/gm.test(username);
+        return !/^[\d-_]|[\d-_]$/.test(username) && !/[^\w-]/.test(username) && !/\d{4}/.test(username);
     }
   }
+
+
